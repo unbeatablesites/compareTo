83de03bf72b929d954b0858306679c6e17eb408e
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-19 11:01:24 -0500

    Practicing compareTo

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,20 @@
 public class App {
 
     public static void main(String[] args) {
-
+        example1();
     }
 
 
+    public static void example1(){
+
+
+        String a = "apple";
+        String b = "apple";
+
+       int ans = a.compareTo("apple");
+
+        System.out.println(ans);
+
+    }
 
 }
