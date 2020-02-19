cc1b486ce99479f4ba285abc9d38dbd43607105a
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-19 11:14:09 -0500

    Practicing compareTo method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,8 @@ public class App {
     public static void main(String[] args) {
         example1();
         example2();
+        example3();
+        example4();
     }
 
 
@@ -14,7 +16,7 @@ public class App {
 
        int ans = a.compareTo(b);
 
-        System.out.println("This returns -4 because all match except 4 chars after match ==> "+ans);
+        System.out.println("This returns 0 because comparing literal string same as object ==> "+ans);
 
     }
     public static void example2(){
@@ -26,6 +28,28 @@ public class App {
         int ans = a.compareTo(b);
 
         System.out.println("This will compare if the two strings are equal. If equal will return int 0 else positive or negative number ==> "+ans);
+
+    }
+    public static void example3(){
+
+
+        String a = "apple";
+        String b = "";
+
+        int ans = a.compareTo(b);
+
+        System.out.println("This is another way to get the length of thr first string. By comparing to an empty string ==> "+ans);
+
+    }
+    public static void example4(){
+
+
+        String a = "apple";
+        String b = "apple";
+
+        int ans = a.compareTo("apple");
+
+        System.out.println("This returns 0 because comparing literal string same as object ==> "+ans);
 
     }
 }
