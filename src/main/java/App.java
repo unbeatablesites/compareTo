086098df224d5bf53c4f357e0d01dba1f1693dcb
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-19 11:08:54 -0500

    Practicing compareTo method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,7 +14,7 @@ public class App {
 
        int ans = a.compareTo(b);
 
-        System.out.println("This will compare if the two strings are equal. If equal will return int 0 else positive or negative number ==> "+ans);
+        System.out.println("This returns -4 because all match except 4 chars after match ==> "+ans);
 
     }
     public static void example2(){
